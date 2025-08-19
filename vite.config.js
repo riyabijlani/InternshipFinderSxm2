a8b28f1a999408371c78b6c6466acfd6b5a17c2e
riyabijlani <riyabijlani14@gmail.com>
2025-08-19 17:59:14 -0400

    Simplify: rely on postcss.config.cjs and ESM-safe vite.config

--- vite.config.js
+++ vite.config.js
@@ -9,12 +9,4 @@ export default defineConfig({
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
-  css: {
-    postcss: {
-      plugins: [
-        require("tailwindcss"),
-        require("autoprefixer"),
-      ],
-    },
-  },
 });
