d3f0a884e6d2e8bd48dfbea45a41197f52f528ea
riyabijlani <riyabijlani14@gmail.com>
2025-08-19 20:06:49 -0400

    Point entry to real main screen (remove placeholder); fix PowerShell compatibility

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 ﻿import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+import App from './pages/index.jsx';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
